615e913ec90f84e6b699e3252346c0e95dc5acd1
“Newriive <newriivedev@gmail.com>
2025-06-04 19:26:09 -0400

    Add nav content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import Footer from '@/components/Footer';
 import { ReactNode } from 'react'
+import Navbar from '@/components/Navbar';
 
 export const metadata = {
   title: 'Newriive',
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <Navbar />
         {children}
         <Footer />
       </body>
